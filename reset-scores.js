a656bab8dea3ec035c34ea31b78b54c55f4032b6
David Meir-Levy <davidmeirlevy@gmail.com>
2019-03-10 23:11:19 +0200

    remove users scores and rooms after reset

--- reset-scores.js
+++ reset-scores.js
@@ -1,5 +1,6 @@
 const {promisify} = require('util');
 const redis = require('redis');
+const {rooms} = require('./model/room');
 const {redisUrl} = require('./config');
 
 const client = redis.createClient(redisUrl);
@@ -7,6 +8,7 @@ const client = redis.createClient(redisUrl);
 
 const setAsync = promisify(client.set).bind(client);
 const getAsync = promisify(client.get).bind(client);
+const keysAsync = promisify(client.keys).bind(client);
 
 
 function set(key, value) {
@@ -18,28 +20,29 @@ function get(key) {
 		.then(JSON.parse);
 }
 
-
-
 async function run() {
-	await Promise.all(['red', 'green', 'orange', 'pink'].map(async room => {
+	await Promise.all(rooms.map(async room => {
 		const key = 'competitors:' + room;
 		return get(key)
 			.then(data => {
-				if(!data) return;
+				if (!data) return;
 				let winner = null;
 				Object.values(data).forEach(obj => {
 					if (!winner || winner.clicks < obj.clicks) {
-						winner = {...obj};
+						winner = obj;
 					}
-					obj.clicks = 0
 				});
 				console.log('winner for ' + room, winner);
-				set(key, data);
+				set(key, []);
 				set('lastWinner:' + room, winner);
 
 				client.publish('newRound', null);
 			});
 	}));
+
+	await keysAsync('users:*').then(users => {
+		console.log('all users:', users);
+	});
 }
 
 run().then(() => process.exit());
